c795c0d43a310f4d4dbb6af2324450d253255ea2
NeliielDW <n.miannay@gmail.com>
2024-03-04 16:17:48 +0100

    Header / Navbar

---
+++ script.js
@@ -0,0 +1,10 @@
+/* NAVBAR RESPONSIVE */
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navbar = document.querySelector('.navbar');
+
+    menuToggle.addEventListener('click', function() {
+        navbar.classList.toggle('open');
+    });
+});
